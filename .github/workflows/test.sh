daa96322ef32431b74bb3ccefaceafe3d68b0ea4
girolle <girolle@github.com>
2021-02-19 14:55:15 +0300

    #

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,5 +1,5 @@
-echo "${ secrets.key }" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
-git clone git@github.com:girolle/autotest.git
-echo "hello"
+echo "${{ secrets.key }}" > "~/.ssh/id_rsa"
+#chmod 600 ~/.ssh/id_rsa
+#cat ~/.ssh/id_rsa
+#git clone git@github.com:girolle/autotest.git
+#echo "hello"
