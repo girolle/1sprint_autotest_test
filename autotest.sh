0d7a95a81e993d2067d67ed5bfcc76394636158a
girolle <girolle@github.com>
2021-02-19 15:48:39 +0300

    again

--- autotest.sh
+++ autotest.sh
@@ -1,7 +1,3 @@
-set -eu
-mkdir $HOME/.ssh
-echo "${{ secrets.key }}" > "$HOME/.ssh/id_rsa"
-chmod 600 ~/.ssh
 sh .github/workflows/test.shgit clone git@github.com:girolle/autotest.git
 ls -la
 echo "hello"
