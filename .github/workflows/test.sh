19357ccde85341fdbfe26b4549fa3ad09ce2f0d7
girolle <girolle@github.com>
2021-02-19 15:09:17 +0300

    try clone

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,6 +1,4 @@
 #touch ~/.ssh/id_rsa
-#echo "${{ secrets.key }}" > "~/.ssh/id_rsa"
-#chmod 600 ~/.ssh/id_rsa
-#cat ~/.ssh/id_rsa
-#git clone git@github.com:girolle/autotest.git
+git clone git@github.com:girolle/autotest.git
+ls -la
 echo "hello"
